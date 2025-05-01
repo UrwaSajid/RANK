501b58c91aca96eb27ebb5e6a49c7c7259c92df0
UrwaSajid <urwasajidvirk@gmail.com>
2025-05-01 20:49:05 +0500

    ui fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     const navigate = useNavigate();
     
     const handleLogin = () => {
-        navigate('/signin');
+        navigate('/signin'); // Navigate to the signin page
     };
     
     const handleRegister = () => {
-        navigate('/signup');
+        navigate('/signup'); // Navigate to the signup page
     };
     
     return (
@@ -32,45 +32,6 @@ const Navbar = () => {
                         </li>
                         <li className="nav-item">
                             <button className="register-btn ms-lg-3" onClick={handleRegister}>Register</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className="login-btn ms-lg-3" onClick={handleLogin}>Login</button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
-};
-
-export default Navbar;
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const Navbar = () => {
-    const navigate = useNavigate();
-    
-    const handleLogin = () => {
-        navigate('/signin');
-    };
-    
-    return (
-        <nav className="navbar navbar-expand-lg">
-            <div className="container">
-                <a className="navbar-brand" href="/">Rank App</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#about">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#faq">FAQ</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#contact">Contact</a>
                         </li>
                         <li className="nav-item">
                             <button className="login-btn ms-lg-3" onClick={handleLogin}>Login</button>
